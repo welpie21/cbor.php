102605a1e9e5f4d0356fb581a5fe4aa3a957af2f
beau <beaudenheijer@gmail.com>
2024-04-09 18:14:59 +0200

    chore: changed tagged value tests

--- tests/cbor/CborTaggedValueTest.php
+++ tests/cbor/CborTaggedValueTest.php
@@ -1,41 +1,47 @@
 <?php
 
+use Beau\CborPHP\CborDecoder;
+use Beau\CborPHP\CborEncoder;
 use Beau\CborPHP\classes\TaggedValue;
+use Beau\CborPHP\exceptions\CborException;
 use PHPUnit\Framework\TestCase;
+
+class UUIDClass
+{
+    public string $uuid = "550e8400-e29b-41d4-a716-446655440000";
+}
 
 class CborTaggedValueTest extends TestCase
 {
     /**
-     * @throws \Beau\CborPHP\exceptions\CborException
+     * @throws CborException
      */
     public function testTaggedValue(): void
     {
-        $data = new TaggedValue(0, "apple");
 
-        $encode = \Beau\CborPHP\CborEncoder::encode($data, function ($key, $value) {
+        $uuid = new UUIDClass();
 
-            var_dump($value);
+        $encode = CborEncoder::encode($uuid, function ($key, $value) {
 
-            if($value instanceof TaggedValue) {
-                return match ($value->tag) {
-                    0 => $value->value . "!",
-                };
+            if ($value instanceof UUIDClass) {
+                return new TaggedValue(7, $value->uuid);
             }
 
             return $value;
         });
 
-        $decode = \Beau\CborPHP\CborDecoder::decode($encode, function ($key, $value) {
+        $decode = CborDecoder::decode($encode, function ($key, $value) {
 
-            if($value instanceof TaggedValue) {
-                return match ($value->tag) {
-                    0 => $value->value,
-                };
+            if($value instanceof TaggedValue && $value->tag === 7) {
+                $uuid = new UUIDClass();
+                $uuid->uuid = $value->value;
+                return $uuid;
             }
 
             return $value;
         });
 
-        $this->assertEquals($data, $decode);
+        $this->assertTrue($decode instanceof UUIDClass);
+        $this->assertEquals($uuid->uuid, $decode->uuid);
     }
 }
